639f56f30e42d4e45eab06165b30977126d445b6
hamanann <a.ahmed_1805@hotmail.com>
2022-10-21 00:20:27 +0200

    Create function that evaluates expression

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    let result = null;
+    switch(operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
